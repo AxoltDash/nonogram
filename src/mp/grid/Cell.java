e77d334c9b5953b1c7c423117f05ce9f4f67a418
AxoltDash <darshan@ciencias.unam.mx>
2024-11-16 11:03:00 -0600

    ✨ Add isFilled and isMarked attributes to Cell

--- src/mp/grid/Cell.java
+++ src/mp/grid/Cell.java
@@ -1,5 +1,23 @@
 package mp.grid;
 
 public class Cell {
+    private boolean isFilled; // [#] or [0]
+    private boolean isMarked; // [X] or [ ]
 
+    public Cell() {
+        this.isFilled = false;
+        this.isMarked = false;
+    }
+
+    public boolean mark() {
+        if (isFilled) {
+            isMarked = true;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isCorrect() {
+        return isFilled && isMarked;
+    }
 }
