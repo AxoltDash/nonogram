aeed3e75431a09f676188a5cd8a1397d040ec302
AxoltDash <darshan@ciencias.unam.mx>
2024-11-16 11:03:16 -0600

    ♻️  Refactor Nonogram class to use Cell objects for nonogram matrix

--- src/mp/grid/Nonogram.java
+++ src/mp/grid/Nonogram.java
@@ -4,22 +4,14 @@ import mp.generator.Generator;
 
 public class Nonogram {
     // Matrices
-    private int[][] nonogram;
-    private int[][] nonogramSolution;
+    private Cell[][] nonogram;
+    private Cell[][] nonogramSolution;
     private int[][] verticalHints;
     private int[][] horizontalHints;
     private int size;
 
-    // User data
-    private int lives;
-    private int score;
-    private int availableHints;
-
     public Nonogram(int size) {
         this.size = size;
-        this.lives = 3;
-        this.score = 0;
-        this.availableHints = 3;
     }
 
     public void generateNonogram() {
@@ -27,6 +19,27 @@ public class Nonogram {
         this.nonogramSolution = generator.generateNonogram(this.size);
         this.verticalHints = generator.generateHints(this.nonogramSolution, false);
         this.horizontalHints = generator.generateHints(this.nonogramSolution, true);
-        this.nonogram = new int[this.size][this.size]; // Empty nonogram
+        this.nonogram = new Cell[this.size][this.size]; // Initialize the nonogram matrix with empty cells
+        for (int i = 0; i < this.size; i++) {
+            for (int j = 0; j < this.size; j++) {
+                this.nonogram[i][j] = new Cell();
+            }
+        }
+    }
+
+    public Cell[][] getNonogram() {
+        return this.nonogram;
+    }
+
+    public Cell[][] getNonogramSolution() {
+        return this.nonogramSolution;
+    }
+
+    public int[][] getVerticalHints() {
+        return this.verticalHints;
+    }
+
+    public int[][] getHorizontalHints() {
+        return this.horizontalHints;
     }
 }
