83a46768b3a06ff710b32f0eb69cf584b98e8560
AxoltDash <darshan@ciencias.unam.mx>
2024-11-16 10:01:20 -0600

    🎉 Enhance main

--- src/mp/Main.java
+++ src/mp/Main.java
@@ -1,7 +1,40 @@
 package mp;
 
+import mp.utils.*;
+
+// =========================================================================
+//  __    _  _______  __    _  _______  _______  ______    _______  __   __ 
+// |  |  | ||       ||  |  | ||       ||       ||    _ |  |   _   ||  |_|  |
+// |   |_| ||   _   ||   |_| ||   _   ||    ___||   | ||  |  |_|  ||       |
+// |       ||  | |  ||       ||  | |  ||   | __ |   |_||_ |       ||       |
+// |  _    ||  |_|  ||  _    ||  |_|  ||   ||  ||    __  ||       ||       |
+// | | |   ||       || | |   ||       ||   |_| ||   |  | ||   _   || ||_|| |
+// |_|  |__||_______||_|  |__||_______||_______||___|  |_||__| |__||_|   |_|
+// =========================================================================
+// Writed by: Pérez Servin Darshan Israel
+// Alias: AxoltDash
+// =========================================================================
+
 public class Main {
-  public static void main(String[] args) {
-    System.out.println("This is the main class");
-  }
+
+    public static void main(String[] args) {
+        // Print the title
+        Colors.title("Welcome to NONOGRAM (Terminal mode)", Colors.CYAN, Colors.MAGENTA);
+    
+        // Build the select label
+        StringBuilder sb = new StringBuilder();
+        sb.append(Colors.hiToString("Select a level (1, 2, or 3):", Colors.CYAN));
+        sb.append("\n");
+        for (int i = 1; i <= 3; i++) {
+            sb.append(Colors.hiToString("[", Colors.WHITE));
+            sb.append(Colors.hiToString(i, Colors.RED));
+            sb.append(Colors.hiToString("] - Level " + i + " ("+ i*5 + " x " + i*5 +").", Colors.WHITE));
+            sb.append("\n");
+        }
+        String selectLabel = sb.toString();
+
+        // Ask for the level
+        int level = Gets.getInt(selectLabel, Colors.toString("Invalid level, try again", Colors.RED), 1, 3);
+    }
 }
+    
