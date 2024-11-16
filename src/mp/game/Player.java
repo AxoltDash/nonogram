de24e117dab2dca665809de847448bf9d881f5d9
AxoltDash <darshan@ciencias.unam.mx>
2024-11-16 10:58:47 -0600

    ✨ Add lives and hints attributes to Player class

--- src/mp/game/Player.java
+++ src/mp/game/Player.java
@@ -3,10 +3,14 @@ package mp.game;
 public class Player {
     private String name;
     private int score;
+    private int lives;
+    private int hints;
 
     public Player(String name) {
         this.name = name;
         this.score = 0;
+        this.lives = 3;
+        this.hints = 3;
     }
 
     public String getName() {
@@ -17,11 +21,25 @@ public class Player {
         return score;
     }
 
+    public int getLives() {
+        return lives;
+    }
+
+    public int getHints() {
+        return hints;
+    }
+
     public void addScore(int score) {
         this.score += score;
     }
 
     public void resetScore() {
         this.score = 0;
-    }   
+    }
+    
+    public void loseLife() {
+        if (this.lives > 0) {
+            this.lives--;
+        }
+    }
 }
