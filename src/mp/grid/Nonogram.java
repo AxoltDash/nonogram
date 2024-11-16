5ee0d41905360ec08c8a5a9ded050a4b92bb418b
AxoltDash <darshan@ciencias.unam.mx>
2024-11-16 10:01:42 -0600

    🚚 Add tools files

---
+++ src/mp/grid/Nonogram.java
@@ -0,0 +1,32 @@
+package mp.grid;
+
+import mp.generator.Generator;
+
+public class Nonogram {
+    // Matrices
+    private int[][] nonogram;
+    private int[][] nonogramSolution;
+    private int[][] verticalHints;
+    private int[][] horizontalHints;
+    private int size;
+
+    // User data
+    private int lives;
+    private int score;
+    private int availableHints;
+
+    public Nonogram(int size) {
+        this.size = size;
+        this.lives = 3;
+        this.score = 0;
+        this.availableHints = 3;
+    }
+
+    public void generateNonogram() {
+        Generator generator = new Generator();
+        this.nonogramSolution = generator.generateNonogram(this.size);
+        this.verticalHints = generator.generateHints(this.nonogramSolution, false);
+        this.horizontalHints = generator.generateHints(this.nonogramSolution, true);
+        this.nonogram = new int[this.size][this.size]; // Initialize the nonogram matrix with zeros
+    }
+}
