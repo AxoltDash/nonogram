3ced83aef570887556eb21052a1af14d6be30bfc
AxoltDash <darshan@ciencias.unam.mx>
2024-11-18 01:06:54 -0600

    🩹 Its works!! But the first row always are = 0

--- src/mp/generator/Generator.java
+++ src/mp/generator/Generator.java
@@ -25,74 +25,60 @@ public class Generator {
             for (int j = 0; j < size; j++) {
                 nonogram[i][j] = new Cell();
             }
+            generateResolvableLine(nonogram[i], size);
         }
-
-        // Generate anchor columns
-        List<Integer> anchorColumns = new ArrayList<>();
-        for (int i = 0; i < size / 2; i++) {
-            anchorColumns.add(random.nextInt(size));
+        for (int j = 0; j < size; j++) {
+            Cell[] column = new Cell[size];
+            for (int i = 0; i < size; i++) {
+                column[i] = nonogram[i][j];
+            }
+            generateResolvableLine(column, size);
         }
-
-        // Generate rows based on anchor columns
-        for (int i = 0; i < size; i++) {
-            generateRow(nonogram, i, size, anchorColumns);
-        }
-
         return nonogram;
     }
 
-    private void generateRow(Cell[][] nonogram, int row, int size, List<Integer> anchorColumns) {
+    private void generateResolvableLine(Cell[] line, int size) {
         int filledCells = 0;
-        List<Integer> blocks = new ArrayList<>();
-
-        // Generate blocks for the row
-        while (filledCells < size) {
-            int maxBlockSize = Math.min(size - filledCells, size / 4); // Limit block size to a quarter of the row size
-            int blockSize = random.nextInt(maxBlockSize) + 1;
-            blocks.add(blockSize);
-            filledCells += blockSize + 1; // +1 for the space between blocks
-        }
-
-        // Adjust blocks to fit the row size
-        while (filledCells > size) {
-            int lastBlock = blocks.remove(blocks.size() - 1);
-            filledCells -= lastBlock + 1;
-        }
-
-        // Place blocks in the row
-        int currentPos = 0;
-        for (int block : blocks) {
-            for (int i = 0; i < block; i++) {
-                nonogram[row][currentPos++].setFilled();
+        int segments = random.nextInt(size / 2) + 1; // Number of segments
+        int[] segmentSizes = new int[segments];
+        for (int i = 0; i < segments; i++) {
+            int maxSegmentSize = size - filledCells - (segments - i - 1);
+            if (maxSegmentSize <= 0) {
+                segmentSizes[i] = 1;
+            } else {
+                segmentSizes[i] = random.nextInt(maxSegmentSize) + 1;
             }
-            if (currentPos < size) {
-                currentPos++; // Skip one cell for the space between blocks
-            }
+            filledCells += segmentSizes[i];
         }
-
-        // Ensure anchor columns are not completely filled
-        for (int col : anchorColumns) {
-            if (nonogram[row][col].isFilled()) {
-                nonogram[row][col].setFilled(false);
+        int emptyCells = size - filledCells;
+        int[] emptySpaces = new int[segments + 1];
+        for (int i = 0; i <= segments; i++) {
+            int maxEmptySpace = emptyCells - (segments - i);
+            if (maxEmptySpace <= 0) {
+                emptySpaces[i] = 1;
+            } else {
+                emptySpaces[i] = random.nextInt(maxEmptySpace) + 1;
             }
+            emptyCells -= emptySpaces[i];
         }
-
-        // Ensure no column is completely filled or empty
-        for (int col = 0; col < size; col++) {
-            if (isColumnFull(nonogram, col, size)) {
-                nonogram[row][col].setFilled(false);
+        int index = 0;
+        for (int i = 0; i < segments; i++) {
+            for (int j = 0; j < emptySpaces[i]; j++) {
+                if (index < size) {
+                    line[index++].setFilled(false);
+                }
+            }
+            for (int j = 0; j < segmentSizes[i]; j++) {
+                if (index < size) {
+                    line[index++].setFilled(true);
+                }
             }
         }
-    }
-
-    private boolean isColumnFull(Cell[][] nonogram, int col, int size) {
-        int filledCount = 0;
-        for (int row = 0; row < size; row++) {
-            if (nonogram[row][col].isFilled()) {
-                filledCount++;
+        for (int j = 0; j < emptySpaces[segments]; j++) {
+            if (index < size) {
+                line[index++].setFilled(false);
             }
         }
-        return filledCount == size;
     }
 
     /**
