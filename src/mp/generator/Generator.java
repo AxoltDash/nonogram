20bc4a29661a61dbd32d9ce19d61dc745ffc1dcd
AxoltDash <darshan@ciencias.unam.mx>
2024-11-18 10:09:17 -0600

    🚧 added prints to check the algoritmn

--- src/mp/generator/Generator.java
+++ src/mp/generator/Generator.java
@@ -20,13 +20,26 @@ public class Generator {
      * @return The generated nonogram.
      */
     public Cell[][] generateNonogram(int size) {
+System.out.println("#####################################");
+        // Inicializa una matriz de celdas del tamaño especificado
         Cell[][] nonogram = new Cell[size][size];
+        
+        // Llena cada fila del nonogram con nuevas celdas y genera líneas resolubles
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 nonogram[i][j] = new Cell();
             }
             generateResolvableLine(nonogram[i], size);
         }
+        // Print of the nonogram
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(nonogram[i][j].isFilled() ? "1" : "0");
+            }
+            System.out.println();
+        }
+
+        // Genera líneas resolubles para cada columna
         for (int j = 0; j < size; j++) {
             Cell[] column = new Cell[size];
             for (int i = 0; i < size; i++) {
@@ -34,24 +47,42 @@ public class Generator {
             }
             generateResolvableLine(column, size);
         }
+        
+System.out.println("--------------");
+        // Print of this for
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(nonogram[i][j].isFilled() ? "1" : "0");
+            }
+            System.out.println();
+        }
+
+System.out.println("#####################################");
         return nonogram;
     }
 
     private void generateResolvableLine(Cell[] line, int size) {
-        int filledCells = 0;
-        int segments = random.nextInt(size / 2) + 1; // Number of segments
-        int[] segmentSizes = new int[segments];
+        int filledCells = 0; // Contador de celdas llenas
+        // Número de segmentos de celdas llenas, por ejemplo, para un tamaño de 10, puede generar entre 1 y 5 segmentos
+        int segments = random.nextInt(size / 2) + 1;
+        int[] segmentSizes = new int[segments]; // Tamaños de los segmentos
+// System.out.println("1 ------------------------------------");
+        // Genera tamaños aleatorios para cada segmento
         for (int i = 0; i < segments; i++) {
             int maxSegmentSize = size - filledCells - (segments - i - 1);
             if (maxSegmentSize <= 0) {
                 segmentSizes[i] = 1;
             } else {
-                segmentSizes[i] = random.nextInt(maxSegmentSize) + 1;
+               segmentSizes[i] = random.nextInt(maxSegmentSize) + 1;
             }
             filledCells += segmentSizes[i];
+// System.out.println("Tamaños aleatorios para cada segmento " + i + " size: " + segmentSizes[i]);
         }
-        int emptyCells = size - filledCells;
-        int[] emptySpaces = new int[segments + 1];
+        
+        int emptyCells = size - filledCells; // Celdas vacías restantes
+        int[] emptySpaces = new int[segments + 1]; // Espacios vacíos entre segmentos
+// System.out.println("2 ------------------------------------");
+        // Genera tamaños aleatorios para los espacios vacíos
         for (int i = 0; i <= segments; i++) {
             int maxEmptySpace = emptyCells - (segments - i);
             if (maxEmptySpace <= 0) {
@@ -60,8 +91,12 @@ public class Generator {
                 emptySpaces[i] = random.nextInt(maxEmptySpace) + 1;
             }
             emptyCells -= emptySpaces[i];
+// System.out.println("Tamaños aleatorios para cada espacio vacio" + i + " size: " + emptySpaces[i]);
         }
-        int index = 0;
+        
+        int index = 0; // Índice para recorrer la línea
+// System.out.println("3 ------------------------------------");
+        // Llena la línea con los espacios vacíos y segmentos generados
         for (int i = 0; i < segments; i++) {
             for (int j = 0; j < emptySpaces[i]; j++) {
                 if (index < size) {
@@ -73,12 +108,18 @@ public class Generator {
                     line[index++].setFilled(true);
                 }
             }
+// System.out.println("Segment " + i + ": " + segmentSizes[i] + " filled cells, " + emptySpaces[i] + " empty cells");
         }
+// System.out.println("4 ------------------------------------");   
+        // Llena el espacio vacío final
         for (int j = 0; j < emptySpaces[segments]; j++) {
             if (index < size) {
-                line[index++].setFilled(false);
+            line[index++].setFilled(false);
             }
         }
+// System.out.println("Final empty space: " + emptySpaces[segments] + " empty cells");
+
+// System.out.println("=====================================");
     }
 
     /**
