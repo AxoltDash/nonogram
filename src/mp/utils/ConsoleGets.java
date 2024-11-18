162c19fb643c720fd719506fa1068b71cf058721
AxoltDash <darshan@ciencias.unam.mx>
2024-11-17 18:22:27 -0600

    🐛 Fix coordinate retrieval order

--- src/mp/utils/ConsoleGets.java
+++ src/mp/utils/ConsoleGets.java
@@ -33,8 +33,8 @@ public class ConsoleGets {
     }
 
     public static int[] getCoordinates(int size, String rowMsg, String colMsg, String error) {
-        int x = getInt(rowMsg, error, 0, size);
-        int y = getInt(colMsg, error, 0, size);
+        int x = getInt(colMsg, error, 0, size);
+        int y = getInt(rowMsg, error, 0, size);
         return new int[] {x, y};
     }
 
